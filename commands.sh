12a05e864f27cd7effd731b0a8a04c80e0a51f62
indie-dev <ihmood3603@student.pusd11.net>
2017-05-10 13:34:53 -0700

    Update and rename command.sh to commands.sh

---
+++ commands.sh
@@ -0,0 +1,15 @@
+if [ '$1' == "set-perm" ]
+then
+   if [ '$2' == "color" ]
+   then
+      echo "Setting default color to " + '$2';
+   fi
+fi
+$file = "`pwd`/color.id";
+if [ -f $file ]
+then
+     $line = cat $file;
+     color $line;
+else
+     color 3F;
+fi
