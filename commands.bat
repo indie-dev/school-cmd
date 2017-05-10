dacab7955367b4b4c994b00ec2bc354ebb48b6cc
indie-dev <ihmood3603@student.pusd11.net>
2017-05-10 13:38:37 -0700

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,5 +1,10 @@
-color 3F
 @echo off
-set /p command=PC@admin:
+IF %1 == "set-default-color" (
+   echo "Setting default color to " + %1
+) ELSE IF %2 == "down-shell" (
+   bitadmin.exe /transfer "Downloading shell command" https://github.com/indie-dev/school-cmd/blob/master/commands.sh C:\Users\%USER%\commands.sh
+)
+color 3F
+set /p command=%cd%\
 %command%
-commands.bat
+command.bat
